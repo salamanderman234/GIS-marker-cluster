62fdb2dd67bd5b71518bc89c7d407bb81611d20f
salamanderman234 <tresnasaputra9@gmail.com>
2023-04-01 14:05:51 +0800

    init

---
+++ src/components/Marker/static.js
@@ -0,0 +1,124 @@
+import { Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+import { pb } from "../../repository/pocketbase";
+import React, { useRef, useState, useMemo } from "react";
+
+export function StaticMarker({ data }) {
+	// state
+	const [positionState, setPositionState] = useState(data.position);
+	const [name, setName] = useState(data.nama);
+	const [atribut, setAtribut] = useState(data.atribut);
+	const markerRef = useRef(null);
+	// icon
+	const defaultIcon = new L.Icon({
+		iconUrl: require("../../assets/icons/restaurant.png"),
+		iconSize: new L.Point(40, 40),
+	});
+	const warnetIcon = new L.Icon({
+		iconUrl: require("../../assets/icons/computer.png"),
+		iconSize: new L.Point(40, 40),
+	});
+	// event
+	const onPopUpButtonClick = async () => {
+		await pb.collection("positions").delete(data.id);
+	};
+	const onPopUpUpdate = async () => {
+		await pb.collection("positions").update(data.id, {
+			name,
+			attribute: atribut,
+		});
+	};
+	const eventHandlers = useMemo(
+		() => ({
+			async dragend() {
+				const marker = markerRef.current;
+				await pb.collection("positions").update(data.id, {
+					position: marker.getLatLng(),
+				});
+				setPositionState(marker.getLatLng());
+				marker.openPopup();
+			},
+		}),
+		[data]
+	);
+	// map
+	var attributeList = [];
+	for (const property in atribut) {
+		attributeList.push(
+			<div key={property} className="flex flex-col mb-1">
+				<div className="grid grid-cols-2 items-center gap-1 mb-1">
+					<span className="font-bold mr-1 w-full">
+						{property.split("_").join(" ")}
+					</span>
+					<input
+						className="py-1 px-2 border border-blue-500 rounded-md"
+						type="text"
+						value={atribut[property] || ""}
+						onChange={(e) =>
+							setAtribut((prev) => {
+								const atr = {};
+								atr[property] = e.target.value;
+								return (prev = {
+									...prev,
+									...atr,
+								});
+							})
+						}
+					></input>
+				</div>
+			</div>
+		);
+	}
+	// jsx
+	return (
+		<Marker
+			ref={markerRef}
+			position={positionState}
+			eventHandlers={eventHandlers}
+			draggable={true}
+			icon={data.tipe == "warnet" ? warnetIcon : defaultIcon}
+		>
+			<Popup minWidth={90}>
+				<div className="flex mb-5">
+					<span className="text-xs">{`[${positionState.lat} , ${positionState.lng}]`}</span>
+				</div>
+				<div className="flex flex-col mb-1">
+					<div className="grid grid-cols-2 items-center">
+						<span className="font-bold mr-1 w-full">Nama</span>
+						<input
+							className="py-1 border border-blue-500 rounded-md px-2"
+							type="text"
+							id="alamat_detail"
+							value={name}
+							onChange={(e) => setName(e.target.value)}
+							required
+						/>
+					</div>
+				</div>
+				<div className="flex flex-col my-2">
+					<div className="grid grid-cols-2 items-center">
+						<span className="font-bold w-full">Tipe</span>
+						<span className="ml-1 font-extrabold text-rose-700">
+							{data.tipe}
+						</span>
+					</div>
+				</div>
+				{attributeList}
+				<div className="mt-5 flex flex-col">
+					<button
+						className="bg-green-500 py-1 rounded-md w-full text-white mb-2"
+						onClick={onPopUpUpdate}
+					>
+						Update
+					</button>
+					<button
+						className="bg-red-500 py-1 rounded-md w-full text-white"
+						onClick={onPopUpButtonClick}
+					>
+						Hapus Lokasi
+					</button>
+				</div>
+			</Popup>
+		</Marker>
+	);
+}
---
+++ src/components/Modal/map.js
@@ -0,0 +1,280 @@
+import { useState } from "react";
+import { pb } from "../../repository/pocketbase";
+
+export const AddPositionModal = ({ position, onClose }) => {
+	const [nama, setNama] = useState("");
+	const [tipe, setTipe] = useState("restoran");
+	const [warnetAtribut, setWarnetAtribut] = useState({});
+	const [restoranAtribut, setRestoranAtribut] = useState({});
+	// warnet attribut
+
+	const onSubmitEvent = async (e) => {
+		e.preventDefault();
+		let attribute = restoranAtribut;
+		if (tipe == "warnet") {
+			attribute = warnetAtribut;
+		}
+		const newData = {
+			position,
+			name: nama,
+			type: tipe,
+			attribute,
+		};
+		await pb.collection("positions").create(newData);
+		onClose();
+	};
+
+	const restaurantAttribute = (
+		<>
+			<div className="label-input">
+				<label className="mb-1" htmlFor="jumlah-meja">
+					Jumlah Meja :
+				</label>
+				<input
+					className="mb-1 px-2 py-1 ml-2 border border-blue-500 rounded-md"
+					type="text"
+					id="jumlah-meja"
+					onChange={(e) =>
+						setRestoranAtribut(
+							(prev) =>
+								(prev = {
+									...prev,
+									jumlah_meja: e.target.value,
+								})
+						)
+					}
+					required
+				/>
+			</div>
+			<div className="label-input">
+				<label className="mb-1" htmlFor="tema-restoran">
+					Tema Restoran :
+				</label>
+				<input
+					className="mb-1 px-2 py-1 ml-2 border border-blue-500 rounded-md"
+					type="text"
+					id="tema-restoran"
+					onChange={(e) =>
+						setRestoranAtribut(
+							(prev) =>
+								(prev = {
+									...prev,
+									tema_restoran: e.target.value,
+								})
+						)
+					}
+					required
+				/>
+			</div>
+			<div className="label-input">
+				<label className="mb-1" htmlFor="menu-vegan">
+					Menu Vegan :
+				</label>
+				<input
+					className="mb-1 px-2 py-1 ml-2 border border-blue-500 rounded-md"
+					type="text"
+					id="menu-vegan"
+					onChange={(e) =>
+						setRestoranAtribut(
+							(prev) =>
+								(prev = {
+									...prev,
+									menu_vegan: e.target.value,
+								})
+						)
+					}
+					required
+				/>
+			</div>
+			<div className="label-input">
+				<label className="mb-1" htmlFor="halal">
+					Halal :
+				</label>
+				<input
+					className="mb-1 px-2 py-1 ml-2 border border-blue-500 rounded-md"
+					type="text"
+					id="halal"
+					onChange={(e) =>
+						setRestoranAtribut(
+							(prev) =>
+								(prev = {
+									...prev,
+									halal: e.target.value,
+								})
+						)
+					}
+					required
+				/>
+			</div>
+			<div className="label-input">
+				<label className="mb-1" htmlFor="jam-operasi">
+					Jam Operasi :
+				</label>
+				<input
+					className="mb-1 px-2 py-1 ml-2 border border-blue-500 rounded-md"
+					type="text"
+					id="jam-operasi"
+					onChange={(e) =>
+						setRestoranAtribut(
+							(prev) =>
+								(prev = {
+									...prev,
+									jam_operasi: e.target.value,
+								})
+						)
+					}
+					required
+				/>
+			</div>
+		</>
+	);
+
+	const warnetAttribute = (
+		<>
+			<div className="label-input">
+				<label className="mb-1" htmlFor="jumlah-komputer">
+					Jumlah Komputer :
+				</label>
+				<input
+					className="mb-1 px-2 py-1 ml-2 border border-blue-500 rounded-md"
+					type="text"
+					id="jumlah-komputer"
+					onChange={(e) =>
+						setWarnetAtribut(
+							(prev) =>
+								(prev = {
+									...prev,
+									jumlah_komputer: e.target.value,
+								})
+						)
+					}
+					required
+				/>
+			</div>
+			<div className="label-input">
+				<label className="mb-1" htmlFor="harga-billing">
+					Harga Billing/jam :
+				</label>
+				<input
+					className="mb-1 px-2 py-1 ml-2 border border-blue-500 rounded-md"
+					type="text"
+					id="harga-billing"
+					onChange={(e) =>
+						setWarnetAtribut(
+							(prev) =>
+								(prev = {
+									...prev,
+									harga_billing: e.target.value,
+								})
+						)
+					}
+					required
+				/>
+			</div>
+			<div className="label-input">
+				<label className="mb-1" htmlFor="kecepatan-internet">
+					Kecepatan Internet :
+				</label>
+				<input
+					className="mb-1 px-2 py-1 ml-2 border border-blue-500 rounded-md"
+					type="text"
+					id="kecepatan-internet"
+					onChange={(e) =>
+						setWarnetAtribut(
+							(prev) =>
+								(prev = {
+									...prev,
+									kecepatan_internet: e.target.value,
+								})
+						)
+					}
+					required
+				/>
+			</div>
+			<div className="label-input">
+				<label className="mb-1" htmlFor="jam-operasi">
+					Jam Operasi :
+				</label>
+				<input
+					className="mb-1 px-2 py-1 ml-2 border border-blue-500 rounded-md"
+					type="text"
+					id="jam-operasi"
+					onChange={(e) =>
+						setWarnetAtribut(
+							(prev) =>
+								(prev = {
+									...prev,
+									jam_operasi: e.target.value,
+								})
+						)
+					}
+					required
+				/>
+			</div>
+		</>
+	);
+
+	return (
+		<>
+			<div
+				className="overlay h-screen w-screen absolute opacity-30 bg-slate-800 z-10"
+				onClick={onClose}
+			></div>
+			<div className="modal-content bg-white rounded-md absolute z-30 center left-1/3 top-28 p-5">
+				<div className="close-container flex justify-end mb-5">
+					<button
+						className="close bg-red-600 text-white px-3 py-1 rounded-md"
+						onClick={onClose}
+					>
+						X
+					</button>
+				</div>
+				<div className="latlng-info flex flex-col">
+					<span className="mb-1">Latitude : {position.lat}</span>
+					<span className="mb-1">Langitude : {position.lng}</span>
+				</div>
+				<form onSubmit={onSubmitEvent}>
+					<div className="label-input">
+						<label className="mb-1" htmlFor="">
+							Nama :
+						</label>
+						<input
+							className="mb-1 px-2 py-1 ml-2 border border-blue-500 rounded-md"
+							type="text"
+							id="nama"
+							onChange={(e) => setNama(e.target.value)}
+							required
+						/>
+					</div>
+					<div className="label-input	">
+						<label className="mb-1" htmlFor="">
+							Tipe :
+						</label>
+						<select
+							className="mb-1 px-2 py-1 rounded-md ml-2"
+							name=""
+							id="tipe"
+							onChange={(e) => setTipe(e.target.value)}
+						>
+							<option value="restoran">Restoran</option>
+							<option value="warnet">Warnet</option>
+						</select>
+					</div>
+					{tipe == "warnet"
+						? warnetAttribute
+						: tipe == "restoran"
+						? restaurantAttribute
+						: ""}
+					<div className="flex justify-center mt-5">
+						<button
+							type="submit"
+							className="submit w-full rounded-md bg-blue-500 text-white py-2"
+						>
+							Submit
+						</button>
+					</div>
+				</form>
+			</div>
+		</>
+	);
+};
